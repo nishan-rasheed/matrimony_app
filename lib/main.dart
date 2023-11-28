2d0c25fab0e5682afd4c0932a6af28bc6d9f48b9
nishan-Art <nishan.artifitia@gmail.com>
2023-11-28 10:19:13 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'matrimony app',
+      title: 'matrimony appssfsfh',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
